fb09bf30a15dfb30e280c99af77f0e762aca1afd
Usmanov Ruslan <usmanovruslan83@gmail.com>
2025-12-01 01:34:18 +0200

    added html

--- main.go
+++ main.go
@@ -9,6 +9,8 @@ func main() {
 
 	mux := http.NewServeMux()
 
+	mux.Handle("/", http.FileServer(http.Dir(".")))
+
 	server := http.Server{Addr: ":8080", Handler: mux}
 
 	log.Fatal(server.ListenAndServe())
