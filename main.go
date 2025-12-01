9ec3679ee87d413a9f95de26c1374ea7a23b759a
Usmanov Ruslan <usmanovruslan83@gmail.com>
2025-12-01 19:50:03 +0200

    added custom handler

--- main.go
+++ main.go
@@ -9,7 +9,18 @@ func main() {
 
 	mux := http.NewServeMux()
 
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	customHandler := func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		w.WriteHeader(200)
+		_, err := w.Write([]byte("OK"))
+		if err != nil {
+			return
+		}
+	}
+
+	mux.HandleFunc("/healthz", customHandler)
+
+	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
 
 	server := http.Server{Addr: ":8080", Handler: mux}
 
