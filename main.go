005bb52c50d5680cf5cfebb151d0fdaa1b0c67c0
Usmanov Ruslan <usmanovruslan83@gmail.com>
2025-12-17 23:51:30 +0200

    added json encoding

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -47,6 +48,40 @@ func main() {
 		}
 	}
 
+	postHandler := func(w http.ResponseWriter, r *http.Request) {
+		type params struct {
+			Body string `json:"body"`
+		}
+
+		type errorResponse struct {
+			Error string `json:"error"`
+		}
+
+		type validResponse struct {
+			Valid bool `json:"valid"`
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+
+		var p params
+		decoder := json.NewDecoder(r.Body)
+		err := decoder.Decode(&p)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			json.NewEncoder(w).Encode(errorResponse{Error: "Something went wrong"})
+			return
+		}
+
+		if len(p.Body) > 140 {
+			w.WriteHeader(http.StatusBadRequest)
+			json.NewEncoder(w).Encode(errorResponse{Error: "Chirp is too long"})
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(validResponse{Valid: true})
+	}
+
 	mux.HandleFunc("GET /api/healthz", customHandler)
 
 	mux.Handle("/app/",
@@ -57,6 +92,8 @@ func main() {
 
 	mux.HandleFunc("GET /admin/metrics", apiCfg.hitHandler)
 	mux.HandleFunc("POST /admin/reset", apiCfg.resetHandler)
+
+	mux.HandleFunc("POST /api/validate_chirp", postHandler)
 
 	server := http.Server{Addr: ":8080", Handler: mux}
 	log.Fatal(server.ListenAndServe())
